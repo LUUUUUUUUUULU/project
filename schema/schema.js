a826e3b501a4569808cc53f92c5394366ceadc53
magicmao <magic_mao@hotmail.com>
2017-05-23 00:20:18 +0800

    updated mlab account

--- schema/schema.js
+++ schema/schema.js
@@ -6,9 +6,9 @@ const mongoose = require('mongoose')
 
 const username = process.env.MONGO_USER || 'testuser'
 const password = process.env.MONGO_PASS || 'password'
-const host = process.env.MONGO_HOST || 'ds147497.mlab.com'
-const port = process.env.MONGO_PORT || 47497
-const database = process.env.MONGO_DB || 'bookshop'
+const host = process.env.MONGO_HOST || 'ds064299.mlab.com'
+const port = process.env.MONGO_PORT || 64299
+const database = process.env.MONGO_DB || 'h370'
 
 mongoose.connect(`mongodb://${username}:${password}@${host}:${port}/${database}`)
 mongoose.Promise = global.Promise
